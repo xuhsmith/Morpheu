e06213b609445e8a4de5b42ecf9e6316c8468356
xuhsmith <33937832+xuhsmith@users.noreply.github.com>
2025-07-09 21:35:05 -0300

    Add files via upload

---
+++ ia_modules/simple_nn.py
@@ -0,0 +1,8 @@
+# Simula o treinamento de um modelo e retorna os "pesos"
+def train_model():
+    weights = {
+        'w1': 0.83,
+        'w2': -0.42,
+        'b': 0.12
+    }
+    return weights
